3bd338c357cece82abdd5dfbda57ad07039d9b58
Philasande Ngubo <142037144+Philasande-Ngubo@users.noreply.github.com>
2025-04-20 10:19:02 +0200

    Main method can now extract data from mnist

--- classifier.py
+++ classifier.py
@@ -2,8 +2,16 @@
 # Date: 20 April 2025
 # Custom Nueral Network
 
+
+from torchvision import datasets
+
 def main():
-    pass
+    Data_DIR = "."
+    train_data = datasets.FashionMNIST(Data_DIR, train = True, download = False)
+    test_data = datasets.FashionMNIST(Data_DIR, train = False, download = False)
+
+    print(train_data)
+    print(test_data)
 
 if __name__ == "__main__":
     main()
