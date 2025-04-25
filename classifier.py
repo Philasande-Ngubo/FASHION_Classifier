f9291a0cc29bfd920e13cf052588dfbf13e7c27e
Philasande Ngubo <142037144+Philasande-Ngubo@users.noreply.github.com>
2025-04-25 10:54:24 +0200

    Configured the device to
    to support gpu if available

--- classifier.py
+++ classifier.py
@@ -3,15 +3,21 @@
 # Custom Nueral Network
 
 
+
+import numpy as np
+import torch
+import torch.nn as nn
 from torchvision import datasets
+import torchvision.transforms as transforms
 
 def main():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     Data_DIR = "."
     train_data = datasets.FashionMNIST(Data_DIR, train = True, download = False)
     test_data = datasets.FashionMNIST(Data_DIR, train = False, download = False)
 
-    print(train_data)
-    print(test_data)
+    labels = ['T-shirts/ top', 'Trouser', 'Pullover', 'Dress','Coat','Sandal','Shirt','Sneaker', 'Bag', 'Ankle boot']
+
 
 if __name__ == "__main__":
     main()
