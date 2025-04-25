70f9e393d89f0e8a588a369009c6062edf5f0b58
Philasande Ngubo <142037144+Philasande-Ngubo@users.noreply.github.com>
2025-04-25 23:26:13 +0200

    Added a forward method to our model to make predictions

--- classifier.py
+++ classifier.py
@@ -18,6 +18,9 @@ Data_DIR = "."
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 LABELS = ['T-shirts/ top', 'Trouser', 'Pullover', 'Dress','Coat','Sandal','Shirt','Sneaker', 'Bag', 'Ankle boot']
 
+def train_model(model):
+    pass
+
 def main():
     
     print("Loading dataset...")
@@ -26,6 +29,48 @@ def main():
     
     train_loader = torch.utils.data.DataLoader( dataset=train_data,batch_size=BATCH_SIZE, shuffle= True)
     test_loader = torch.utils.data.DataLoader( dataset=test_data,batch_size=BATCH_SIZE, shuffle= False)
+
+    model = ForwardNueralNetwork(INPUT_SIZE, HIDDEN_SIZE, NUM_CLASSES)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr = LEARNING_RATE)
+
+    # training 
+    n_steps = len(train_loader)
+    for epoch in range(NUM_EPOCHS):
+        for i, (x, true_labels) in enumerate(train_loader):
+            x = x.reshape(-1, 28*28).to(DEVICE)
+            true_labels = true_labels.to(DEVICE)
+
+            predicted_labels = model(x)
+            loss = criterion(predicted_labels, true_labels)
+
+            optimizer.zero_grad()
+            loss.backwards()   #back propagation
+
+            optimizer.step()
+
+            if ((i+1) % HIDDEN_SIZE == 0):
+                print(f'Epoch {epoch} of {NUM_EPOCHS} , Loss = {loss.item():.4f}')
+    
+    with torch.no_grad():
+        num_correct_predictions = 0
+        testing_data_size = 0
+        for i, (x, true_labels) in enumerate(test_loader):
+             x = x.reshape(-1, 28*28).to(DEVICE)
+             true_labels = true_labels.to(DEVICE)
+
+             predicted_labels = model(x)
+
+             _, predictions = torch.max(predicted_labels, 1)
+             testing_data_size += true_labels.shape[0]
+             num_correct_predictions += (predictions == true_labels).sum().item()
+        
+        model_accuracy = 100.0 * ( num_correct_predictions/ testing_data_size)
+        print(f'Model accurracy {model_accuracy:.2f}%')
+
+
+
 
 
 if __name__ == "__main__":
