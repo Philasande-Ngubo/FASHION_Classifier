3204ead64f37403e6f833ded88dfe26e0fe84e3b
Philasande Ngubo <142037144+Philasande-Ngubo@users.noreply.github.com>
2025-04-25 21:50:19 +0200

    Added data loaders for the test and training dataset

--- classifier.py
+++ classifier.py
@@ -13,6 +13,7 @@ HIDDEN_SIZE = 100
 NUM_CLASSES = 10
 NUM_EPOCHS = 3
 LEARNING_RATE = 0.001
+BATCH_SIZE = 32
 Data_DIR = "."
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 LABELS = ['T-shirts/ top', 'Trouser', 'Pullover', 'Dress','Coat','Sandal','Shirt','Sneaker', 'Bag', 'Ankle boot']
@@ -21,9 +22,9 @@ def main():
     
     train_data = datasets.FashionMNIST(Data_DIR, train = True, download = False, transform = transforms.ToTensor())
     test_data = datasets.FashionMNIST(Data_DIR, train = False, download = False, transform = transforms.ToTensor())
-
-
     
+    train_loader = torch.utils.data.DataLoader( dataset=train_data,batch_size=BATCH_SIZE, shuffle= True)
+    test_loader = torch.utils.data.DataLoader( dataset=test_data,batch_size=BATCH_SIZE, shuffle= False)
 
 
 if __name__ == "__main__":
