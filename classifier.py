94f5868d128d54ad37a3accca1e3d8732fe27d1b
Philasande Ngubo <142037144+Philasande-Ngubo@users.noreply.github.com>
2025-04-25 11:04:48 +0200

    Transformed the train_data and test_data to tensors

--- classifier.py
+++ classifier.py
@@ -2,21 +2,28 @@
 # Date: 20 April 2025
 # Custom Nueral Network
 
-
-
 import numpy as np
 import torch
 import torch.nn as nn
 from torchvision import datasets
 import torchvision.transforms as transforms
 
-def main():
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    Data_DIR = "."
-    train_data = datasets.FashionMNIST(Data_DIR, train = True, download = False)
-    test_data = datasets.FashionMNIST(Data_DIR, train = False, download = False)
+INPUT_SIZE = 784
+HIDDEN_SIZE = 100
+NUM_CLASSES = 10
+NUM_EPOCHS = 3
+LEARNING_RATE = 0.001
+Data_DIR = "."
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+LABELS = ['T-shirts/ top', 'Trouser', 'Pullover', 'Dress','Coat','Sandal','Shirt','Sneaker', 'Bag', 'Ankle boot']
 
-    labels = ['T-shirts/ top', 'Trouser', 'Pullover', 'Dress','Coat','Sandal','Shirt','Sneaker', 'Bag', 'Ankle boot']
+def main():
+    
+    train_data = datasets.FashionMNIST(Data_DIR, train = True, download = False, transform = transforms.ToTensor())
+    test_data = datasets.FashionMNIST(Data_DIR, train = False, download = False, transform = transforms.ToTensor())
+
+
+    
 
 
 if __name__ == "__main__":
